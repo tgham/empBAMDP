19beeecd4ca12241f86ff17788534ca56c651ac9
Tom Graham <tgraham1998@outlook.com>
2026-06-02 16:24:54 +0200

    .

--- ell_alpha_sweep.py
+++ ell_alpha_sweep.py
@@ -17,6 +17,15 @@ def main():
     args = parser.parse_args()
 
     ## run expt
+    print('Running experiment with parameters:')
+    print(f'  n_arms: {args.n_arms}')
+    print(f'  n_outcomes: {args.n_outcomes}')
+    print(f'  n_trials: {args.n_trials}')
+    print(f'  n_ell_samples: {args.n_ell_samples}')
+    print(f'  ell_lo: {args.ell_lo}')
+    print(f'  ell_hi: {args.ell_hi}')
+    print(f'  alphas: {args.alphas}')
+    print(f'  termination_arm: {args.termination_arm}')
     df_curves = enumerate_curves(n_arms=args.n_arms, n_outcomes=args.n_outcomes, n_trials=args.n_trials, alphas=args.alphas,
                                  ell_hi=args.ell_hi, ell_lo=args.ell_lo,
                                  termination_arm=args.termination_arm,
@@ -25,3 +34,6 @@ def main():
     
     ## save
     df_curves.to_csv(f'useful_saves/sweep/{args.n_arms}arms_{args.n_outcomes}outcomes_{args.n_trials}trials.csv', index=False)
+
+if __name__ == '__main__':
+    main()
